5b996f967085440f8f3cf9d4df09a0fd70e0f612
Anna Manole <am224wd@student.lnu.se>
2025-07-23 17:08:35 +0200

    iterate through the object w-o spreading it

--- src/custom-components/header.tsx
+++ src/custom-components/header.tsx
@@ -3,40 +3,39 @@ import { useLocation } from "react-router";
 
 export default function Header() {
   const location = useLocation();
+  console.log(location.pathname);
 
-  const links = [
+  const paths = [
     { href: "/", label: "Home" },
     { href: "/about", label: "About" },
     { href: "/google", label: "Login with Google" },
   ];
-
   return (
     <header>
       <nav>
         <ul className="flex justify-between items-center">
           <Flower />
           <div className="flex gap-10 items-center">
-            {links.map(({ href, label }) => {
-              const isActive = location.pathname === href;
-
+            {paths.map((path) => {
+              const isActive = path.href === location.pathname;
               return (
-                <li key={href}>
+                <li key={path.href}>
                   <a
-                    href={href}
+                    href={path.href}
                     className="group flex items-center space-x-2 text-base"
                   >
-                    <Flower
-                      size={18}
-                      className={`transition-all duration-200 mt-0.5
-                        ${
-                          isActive
-                            ? "text-green"
-                            : "opacity-0 group-hover:opacity-100"
-                        }
-                        group-hover:text-current
-                      `}
-                    />
-                    <span>{label}</span>
+                    <span>
+                      <Flower
+                        size={16}
+                        className={`transition-all duration-200
+                          ${
+                            isActive
+                              ? "opacity-100 text-green group-hover:text-current"
+                              : "opacity-0 group-hover:opacity-100"
+                          }`}
+                      />
+                    </span>
+                    <span>{path.label}</span>
                   </a>
                 </li>
               );
