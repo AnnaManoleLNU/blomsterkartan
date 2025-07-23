0cc0a512e036597cd9617377525f4685f0a58431
Anna Manole <am224wd@student.lnu.se>
2025-07-23 14:55:18 +0200

    fix adding colors

--- src/custom-components/hero.tsx
+++ src/custom-components/hero.tsx
@@ -3,18 +3,18 @@ import { Search } from "lucide-react";
 
 export default function Hero() {
   return (
-    <div className="flex justify-center h-screen bg-fog ">
+    <section className="flex justify-center h-screen bg-fog ">
       <div className="flex justify-center flex-col text-center">
         <h1 className="text-green text-6xl">Blomsterkartan</h1>
         <p className="text-blue text-2xl">Upptäck Sveriges blommor</p>
         <div className=" flex relative">
           <Input
             placeholder="Sök efter blommor..."
-            className=" border-blue rounded-full"
+            className="border-blue rounded-full"
           />
           <Search className="h-full text-blue absolute right-2" />
         </div>
       </div>
-    </div>
+    </section>
   );
 }
