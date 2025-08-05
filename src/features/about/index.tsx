41c6cc8f76fa1c90e1d703445f02dad4fa1ecab9
Anna Manole <am224wd@student.lnu.se>
2025-08-05 10:43:14 +0200

    test upload a file

--- src/features/about/index.tsx
+++ src/features/about/index.tsx
@@ -1,10 +1,52 @@
-
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import type { PutBlobResult } from "@vercel/blob";
+import { useState, useRef } from "react";
 
 export default function About() {
-
+  const inputFileRef = useRef<HTMLInputElement>(null);
+  const [blob, setBlob] = useState<PutBlobResult | null>(null);
   return (
     <>
-      
+      <h1>Upload Picture</h1>
+
+      <form
+        onSubmit={async (event) => {
+          event.preventDefault();
+
+          if (!inputFileRef.current?.files) {
+            throw new Error("No file selected");
+          }
+
+          const file = inputFileRef.current.files[0];
+
+          const response = await fetch(
+            `/api/upload?filename=${encodeURIComponent(file.name)}`,
+            {
+              method: "POST",
+              body: file,
+            }
+          );
+
+          const newBlob = (await response.json()) as PutBlobResult;
+
+          setBlob(newBlob);
+        }}
+      >
+        <Input
+          name="file"
+          ref={inputFileRef}
+          type="file"
+          accept="image/jpeg, image/png, image/webp"
+          required
+        />
+        <Button type="submit">Upload</Button>
+      </form>
+      {blob && (
+        <div>
+          Blob url: <a href={blob.url}>{blob.url}</a>
+        </div>
+      )}
     </>
   );
 }
