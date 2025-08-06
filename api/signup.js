a9615fb05d0815a2ed12476c8e0ba7e5327eb86c
Anna Manole <am224wd@student.lnu.se>
2025-08-06 12:45:41 +0200

    add username to registration

--- api/signup.js
+++ api/signup.js
@@ -4,14 +4,26 @@ import bcrypt from "bcrypt";
 const prisma = new PrismaClient();
 
 export default async function handler(req, res) {
-  if (req.method !== 'POST') return res.status(405).end()
+  if (req.method !== "POST") return res.status(405).end();
 
-  const { email, password } = req.body
-  const existingUser = await prisma.user.findUnique({ where: { email } })
-  if (existingUser) return res.status(400).json({ error: 'Email already exists.' })
+  const { username, email, password } = req.body;
+  const existingUser = await prisma.user.findFirst({
+    where: {
+      OR: [{ email: email }, { username: username }],
+    },
+  });
 
-  const hashed = await bcrypt.hash(password, 10)
-  const user = await prisma.user.create({ data: { email, password: hashed } })
+  if (existingUser)
+    return res.status(400).json({ error: "Email or username already exists." });
 
-  res.status(201).json({ user: { id: user.id, email: user.email } })
+  const hashed = await bcrypt.hash(password, 10);
+  const user = await prisma.user.create({
+    data: { username, email, password: hashed },
+  });
+
+  res
+    .status(201)
+    .json({
+      user: { id: user.id, email: user.email, username: user.username },
+    });
 }
